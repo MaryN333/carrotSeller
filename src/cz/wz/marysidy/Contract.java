57a31f04430bd5edfac1266846243b7a0f526cc8
MaryN333 <sidyakina@gmail.com>
2024-09-27 10:25:55 +0200

    Created Contract class for managing agreements. Closes #2

---
+++ src/cz/wz/marysidy/Contract.java
@@ -0,0 +1,51 @@
+package cz.wz.marysidy;
+
+import java.time.LocalDate;
+
+public class Contract {
+    private String contractNumber;
+    private LocalDate signingDate;
+    private double weightInTons;
+
+    // Constructor
+    public Contract(String contractNumber, LocalDate signingDate, double weightInTons) {
+        this.contractNumber = contractNumber;
+        this.signingDate = signingDate;
+        this.weightInTons = weightInTons;
+    }
+
+    // Getters and Setters
+    public String getContractNumber() {
+        return contractNumber;
+    }
+
+    public void setContractNumber(String contractNumber) {
+        this.contractNumber = contractNumber;
+    }
+
+    public LocalDate getSigningDate() {
+        return signingDate;
+    }
+
+    public void setSigningDate(LocalDate signingDate) {
+        this.signingDate = signingDate;
+    }
+
+    public double getWeightInTons() {
+        return weightInTons;
+    }
+
+    public void setWeightInTons(double weightInTons) {
+        this.weightInTons = weightInTons;
+    }
+
+    // Methods
+    @Override
+    public String toString() {
+        return "Contract{" +
+                "contractNumber='" + contractNumber + '\'' +
+                ", signingDate=" + signingDate +
+                ", weightInTons=" + weightInTons +
+                '}';
+    }
+}
